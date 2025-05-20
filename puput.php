2cef0cbf3deeef9dd8c56b0e764c31351a511bfd
RayhanPradana <rayhanschool20@gmail.com>
2025-05-20 12:07:52 +0700

    add all page v.alpha 01

---
+++ puput.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <title>Puput</title>
+</head>
+<body>
+    <h1>Puput Galuh</h1>
+</body>
+</html>
---
+++ risqy.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <title>Rizqy</title>
+</head>
+<body>
+    <h1>Rizqy Eka</h1>
+</body>
+</html>
